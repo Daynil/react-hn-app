f25c63e2efed1fe433a78476829024b7b7002423
Danny Libin <Danny Libin>
2017-08-09 21:12:25 -0500

    material styles added

--- src/components/hot/HotPage.js
+++ src/components/hot/HotPage.js
@@ -1,15 +1,48 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
+import * as storiesActions from '../../actions/storiesActions';
+import {Link, withRouter} from 'react-router-dom';
+
+import './HotePage.css';
+import Card, {CardContent} from 'material-ui/Card';
+import IconButton from 'material-ui/IconButton';
+import Typography from 'material-ui/Typography';
+
 
 class HotPage extends React.Component {
+
   render() {
+    const storyCards = this.props.stories.map((story, i) => {
+      return (
+        <Card key={i}>
+          <CardContent>
+            <Typography type="headline">{story.title}</Typography>
+          </CardContent>
+        </Card>
+      );
+    });
+
     return (
-      <div>
-        This is the hot page.
+      <div id="page-wrapper">
         <Link to="top">Goto top page</Link>
+        {storyCards}
       </div>
     );
   }
 }
 
-export default HotPage;
+function mapStateToProps(state, ownProps) {
+  return {
+    stories: state.stories
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    actions: bindActionCreators(storiesActions, dispatch)
+  };
+}
+
+
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(HotPage));
